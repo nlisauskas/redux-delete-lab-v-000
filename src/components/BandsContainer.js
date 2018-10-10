482fe7959961bb00942287c6a0c8986bbe80729e
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-10-10 01:59:07 +0000

    automatically backed up by learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import BandInput from './BandInput';
-
+import Band from './Band';
 import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
